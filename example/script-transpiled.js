45d7b386dfc38b8c69b0b8e180045e49fee737b3
Sunny <sjbeatteay@gmail.com>
2017-11-26 21:16:50 -0500

    update .gitignore

---
+++ example/script-transpiled.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _conclaveTranspiled = require('../conclave-transpiled');
+
+var _conclaveTranspiled2 = _interopRequireDefault(_conclaveTranspiled);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+new _conclaveTranspiled2.default();
